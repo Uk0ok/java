418edeee3b472024c66a02366a426fbca5cddd9e
DESKTOP-RUOFDOG\Uk0ok <jaewook211@naver.com>
2023-02-27 18:02:27 +0900

    20230227

--- src/inflearn/chapter5/Student.java
+++ src/inflearn/chapter5/Student.java
@@ -6,8 +6,23 @@ public class Student {
     int grade;
     String address;
 
+    public Student() {} // 디폹트 생성자
+
+    public Student(int id, String name) {
+        studentID = id;
+        studentName = name;
+    }
+
     public void showStudentinfo() {
         System.out.println(studentName + ", " + address);
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
     }
 } 
 
--- src/inflearn/chapter5/StudentTest.java
+++ src/inflearn/chapter5/StudentTest.java
@@ -2,10 +2,19 @@ package inflearn.chapter5;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.studentName = "이순신";
-        student.address = "서울시 영등포구";
+        
+        // Student studentLee = new Student(100, "이순신"); 
+        Student studentLee = new Student(); 
+        studentLee.studentName = "이순신";
+        studentLee.studentID = 100;
+        studentLee.address = "서울시 영등포구";
+        
+        Student studentKim = new Student(101, "김유신"); 
+        // studentKim.studentName = "김유신";
+        // studentKim.studentID = 101;
+        // studentKim.address = "서울시 관악구";
 
-        student.showStudentinfo();
+        studentLee.showStudentinfo();
+        studentKim.showStudentinfo();
     }
 }
